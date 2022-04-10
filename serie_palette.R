3a2f06d7305dc7fb0162c4562483bce2f820bd26
Jesus MQQ <50846818+JesusRQP96@users.noreply.github.com>
2022-04-10 17:36:31 -0500

    palette 1.0

---
+++ serie_palette.R
@@ -0,0 +1,48 @@
+serie_palette <- function(name,palette_color,n, type = c("discrete", "continuous")) {
+  
+  pal <- palette_color[name][[1]]
+  
+  
+  if (is.null(pal)){ 
+    stop("Palette not found.")
+  }
+  
+  if (missing(n)) { 
+    n <- length(pal)
+  }
+  
+  if (missing(type)) {
+    if(n > length(pal)){type <- "continuous"}
+    else{ type <- "discrete"}
+  }
+  type <- match.arg(type)
+  
+  
+  if (type == "discrete" && n > length(pal)) {
+    stop("Number of requested colors greater than what discrete palette can offer, \n  use as continuous instead.")
+  }
+  
+  
+  out <- switch(type,
+                continuous = grDevices::colorRampPalette(pal[1:3])(n),
+
+                discrete = pal[c(1:n)],
+  )
+  structure(out, class = "serie_palette", name = name)
+}
+
+
+print.serie_palette <- function(x, ...) {
+  pallength <- length(x)
+  latinpar <- par(mar=c(0.25,0.25,0.25,0.25))
+  on.exit(par(latinpar))
+  
+  image(1:pallength, 1,
+        as.matrix(1:pallength),
+        col = x,
+        axes=FALSE)
+  
+  text(median(1:pallength), 1,
+       labels = paste0(attr(x,"name"),", n=",pallength),
+       cex = 3, family = "sans")
+}
