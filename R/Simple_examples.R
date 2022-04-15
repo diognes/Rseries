a2a7bd8fc9ccf096d21c6ddada67d33c0a76dc44
Dio Rod <rod_nm@outlook.com>
2022-04-15 16:44:32 -0500

    Create Simple_examples.R
    Simple examples of how the function works

---
+++ R/Simple_examples.R
@@ -0,0 +1,24 @@
+# Simple examples
+
+# palette_netflyx
+serie_palette("Lupin", palette_netflyx, n=4, type="discrete")
+
+serie_palette("Lupin", palette_netflyx, 9)
+
+serie_palette("Lupin", palette_netflyx, 20)
+
+serie_palette("Lupin", palette_netflyx, 50, type="continuous")
+
+# palette_anime
+serie_palette("poke", palette_anime, n=4, type="discrete")
+
+serie_palette("poke", palette_anime, 9)
+
+serie_palette("poke", palette_anime, 20)
+
+serie_palette("poke", palette_anime, 50, type="continuous")
+
+
+
+
+
