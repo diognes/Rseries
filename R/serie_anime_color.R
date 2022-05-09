e8d4e40fa9a7c739f55c67e3c276619bea26633f
Dio Rod <rod_nm@outlook.com>
2022-05-08 21:57:17 -0500

    Update serie_anime_color.R

--- R/serie_anime_color.R
+++ R/serie_anime_color.R
@@ -28,29 +28,29 @@ palette_anime = list(
   
   mobu2 = c("#F0ED36", "#EA8638", "#49B47F", "#05EEFF", "#DAC2A5", "#EAEAE8"), #6
   
-  lagann_drill = c("#DE4FD3", "#D28883", "#F1D29C", "#FE7934", "#C82523", "#335F9E"), #6
+  # lagann_drill = c("#DE4FD3", "#D28883", "#F1D29C", "#FE7934", "#C82523", "#335F9E"), #6
   
   asobi = c("#B1C9CA", "#F3E4D3", "#CDBCAC", "#A1884D", "#D7E5A9", "#B1CB7A"), #6
   
   sng = c("#37E4E3", "#118F83", "#A72B19", "#F25039", "#DC7E2A", "#E6C6A1"), #6
   
-  lakill = c("#534A5B", "#628591", "#926949", "#F9CDC2", "#E63E40", "#8E2021"), #6
+  # lakill = c("#534A5B", "#628591", "#926949", "#F9CDC2", "#E63E40", "#8E2021"), #6
   
-  ggo = c("#F47DA5", "#EFB093", "#DC745B", "#C5776A", "#B05259", "#7E2D2F"), #6
+  ggo = c("#D17F74", "#F4EBE2", "#832E43", "#E97798", "#563C47", "#BD9EAE"), #6
   
-  naru = c("#264F94", "#993A42", "#E33329", "#E98352", "#EEC1BD", "#DFDE4E"), #6
+  # naru = c("#264F94", "#993A42", "#E33329", "#E98352", "#EEC1BD", "#DFDE4E"), #6
   
-  narushi = c("#962940", "#AD613F", "#E6A14B", "#4496D2", "#85A86E", "#ECB9A6"), #6
+  # narushi = c("#962940", "#AD613F", "#E6A14B", "#4496D2", "#85A86E", "#ECB9A6"), #6
   
   bolt = c("#F2C3EB", "#543BFC", "#D95F9C", "#EAA85A", "#BB7EDE", "#FB7318"), #6
   
   yuri = c("#264268", "#62ABDB", "#A5E5FB", "#FEFEFE", "#849CBB", "#42C7C8"), #6
   
-  hitman = c("#CE7368", "#FAD461", "#EBE7F6", "#DAC0F0", "#7585C1", "#564846"), #6
+  # hitman = c("#CE7368", "#FAD461", "#EBE7F6", "#DAC0F0", "#7585C1", "#564846"), #6
   
-  hataraku = c("#758267", "#DA6557", "#E6E7E5", "#99B1BF", "#794B41", "#C7C2C6"), #6
+  # hataraku = c("#DA6557", "#C7C2C6", "#794B41", "#E6E7E5", "#99B1BF", "#CAEFF7"), #6
   
-  nichi = c("#7B9CC0", "#E8969A", "#C6737F", "#F2F2F2", "#F8E9C2", "#C2967D"), #6
+  # nichi = c("#7B9CC0", "#E8969A", "#C6737F", "#F2F2F2", "#F8E9C2", "#C2967D"), #6
   
   yuru2 = c("#9F5C66", "#674D4A", "#F9F7F6", "#DCC4C2", "#6FB8E0", "#B4B5CA"), #6
   
@@ -60,10 +60,10 @@ palette_anime = list(
   
   takt = c("#5FA1B1", "#727680", "#FBEFEF", "#C59EA6", "#C34B53", "#C5C3C1"), #6
   
-  cardcaptor = c("#DD9D54", "#6E3327", "#97D0EA", "#EC494F", "#F8E1E7", "#FBCA67"), #6
+  cardcaptor = c("#DD9D54", "#6E3327", "#97D0EA", "#EC494F", "#F8E1E7", "#FBCA67") #6
   
-  cardcaptor_clear = c("#3278CC", "#567EB7", "#E96388", "#E3B277", "#E9D9C3", "#F8F3F0"), #6
+  # cardcaptor_clear = c("#3278CC", "#567EB7", "#E96388", "#E3B277", "#E9D9C3", "#F8F3F0"), #6
   
-  fmab = c("#F9F0C4", "#A7A8AC", "#5F413D", "#C2A68C", "#535959", "#C86D6A") #6
+  # fmab = c("#F9F0C4", "#A7A8AC", "#5F413D", "#C2A68C", "#535959", "#C86D6A") #6
   
 )
