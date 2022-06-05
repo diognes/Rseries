d8df39d35708735700865545a18354922be488db
Jesus MQQ <50846818+JesusRQP96@users.noreply.github.com>
2022-06-05 11:08:16 -0500

    Create serie_peru_color.R

---
+++ R/serie_peru_color.R
@@ -0,0 +1,16 @@
+# 6.Create the color palettes
+#::::::::::::::::::::::::::::::::::::::::::::::
+#' Complete list of palettes
+#'
+#' Use \code{names(palette_peru)} to view list of palette netflyx names.
+#'
+#' @export
+
+
+
+palette_peru <- list(
+
+      GranSangre = c("#c6171a","#711f1d","#531e1b","#502526","#3f221c","#2c211b")
+
+
+) 
