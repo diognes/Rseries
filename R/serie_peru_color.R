d26831bc79c71df066499c13a23fd3531fec46d4
Jesus MQQ <50846818+JesusRQP96@users.noreply.github.com>
2022-06-05 12:48:57 -0500

    palettes

--- R/serie_peru_color.R
+++ R/serie_peru_color.R
@@ -10,7 +10,12 @@
 
 palette_peru <- list(
 
-      GranSangre = c("#c6171a","#711f1d","#531e1b","#502526","#3f221c","#2c211b")
+      GranSangre      = c("#c6171a","#711f1d","#531e1b","#502526","#3f221c","#2c211b"),
+      AlFondoHaySitio = c("#c77c10","#f3a316","#035263","#0077a5","#105a00","#32880e"),
+      Graffitti       = c("#2b5aab","#1a79b8","#0396d0","#bada80","#98be58","#93af3b"),
+      LaPre           = c("#5f6b67","#3b4440","#ea61b2","#c52984","#51e6e3","#27b8bc"),
+      MilOficios      = c("#719a4f","#85bc55","#adde6b","#fc8577","#c64034","#a53e30"),
+      QueBuenaRaza    = c("#e92567","#f6739f","#ffa5c4","#6abce2","#5fb1df","#3f91d0")
 
 
 ) 
