0670c311f7986292e04c87ea620f1d9fcb0d1daf
rodnm <rod_nm@outlook.com>
2022-06-05 16:31:25 -0500

    update

--- R/serie_anime_color.R
+++ R/serie_anime_color.R
@@ -7,63 +7,22 @@
 #' @export
 
 palette_anime = list(
-  
-  #logos
-  digi_frontier = c("#1482B9", "#FFFFFF", "#FD3C04", "#FEB301", "#CCA001", "#FF7E02"), #6
-  
-  tokyog = c("#7BD9D8", "#FE139E", "#FFEA00", "#E90921", "#20B5C8", "#0048EC"), #6
-  
-  poke = c("#FFCC01", "#2C6BB4", "#98954C", "#577887", "#355FAC", "#C8A102"), #6
-  
-  fmab_l = c("#CACBCD", "#4A4A4B", "#D01820", "#76777B", "#5C5C5B", "#938B90"), #6
-  
-  pocketomonsta = c("#58B770", "#C1E8A7", "#0C280E", "#156621", "#157F28", "#9CE6B4"), #8
-  
-  poke = c("#FFCC01", "#2C6BB4", "#98954C", "#577887", "#355FAC", "#C8A102"), #6
-
-  yuyuh = c("#F8D03D", "#FCE3D2", "#EC7C54", "#EC5F36", "#EC3C24", "#E40414"), #6
 
   #posters
   poputepik =c("#0045A0", "#FDDEAE", "#DE7B7C", "#EFC6AF", "#A1B395", "#E7180E"), #6
   
-  mobu2 = c("#F0ED36", "#EA8638", "#49B47F", "#05EEFF", "#DAC2A5", "#EAEAE8"), #6
-  
-  # lagann_drill = c("#DE4FD3", "#D28883", "#F1D29C", "#FE7934", "#C82523", "#335F9E"), #6
-  
   asobi = c("#B1C9CA", "#F3E4D3", "#CDBCAC", "#A1884D", "#D7E5A9", "#B1CB7A"), #6
   
-  sng = c("#37E3E3", "#118F83", "#E6C6A0", "#83431D", "#F25039", "#A62B19"), #6
-  
-  # lakill = c("#534A5B", "#628591", "#926949", "#F9CDC2", "#E63E40", "#8E2021"), #6
-  
   ggo = c("#D17F74", "#F4EBE2", "#832E43", "#E97798", "#563C47", "#BD9EAE"), #6
-  
-  naru = c("#264F94", "#993A42", "#E33329", "#E98352", "#EEC1BD", "#DFDE4E"), #6
-  
-  # narushi = c("#962940", "#AD613F", "#E6A14B", "#4496D2", "#85A86E", "#ECB9A6"), #6
   
   bolt = c("#F2C3EB", "#543BFC", "#D95F9C", "#EAA85A", "#BB7EDE", "#FB7318"), #6
   
   yuri = c("#264268", "#62ABDB", "#A5E5FB", "#42C7C8", "#FEFEFE", "#849CBB"), #6
-  
-  hitman = c("#CE7368", "#FAD461", "#EBE7F6", "#DAC0F0", "#7585C1", "#564846"), #6
-  
-  hataraku = c("#DA6557", "#C7C2C6", "#794B41", "#E6E7E5", "#99B1BF", "#CAEFF7"), #6
-  
-  nichi = c("#7B9CC0", "#E8969A", "#C6737F", "#F2F2F2", "#F8E9C2", "#C2967D"), #6
-  
-  yuru2 = c("#9F5C66", "#674D4A", "#F9F7F6", "#DCC4C2", "#6FB8E0", "#B4B5CA"), #6
   
   tokyorev = c("#D81709", "#F7E39B", "#DEB279", "#FAE6CB", "#E74417", "#59221A"), #6
   
   yona = c("#734243", "#E6B2B0", "#AE4266", "#F6E1E4", "#C18C6C", "#D9CAD1"), #6
   
   takt = c("#727681", "#FBEFEF", "#C34B54", "#DFAFBB", "#A27E5C", "#EEB987"), #6
-  # takt = c("#5FA1B1", "#727680", "#FBEFEF", "#C59EA6", "#C34B53", "#C5C3C1"), #6
-  cardcaptor = c("#DD9D54", "#6E3327", "#97D0EA", "#EC494F", "#F8E1E7", "#FBCA67") #6
-  
-  # cardcaptor_clear = c("#3278CC", "#567EB7", "#E96388", "#E3B277", "#E9D9C3", "#F8F3F0"), #6
-  
-  # fmab = c("#F9F0C4", "#A7A8AC", "#5F413D", "#C2A68C", "#535959", "#C86D6A") #6
-  
+
 )
