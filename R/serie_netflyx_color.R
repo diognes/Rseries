2fa47a03f6c34a1c48b8d99f3b47187e91e34fec
Jesus MQQ <50846818+JesusRQP96@users.noreply.github.com>
2022-06-05 10:28:39 -0500

    netflyx actualization

--- R/serie_netflyx_color.R
+++ R/serie_netflyx_color.R
@@ -8,6 +8,7 @@
 
 
 palette_netflyx <- list(
+  
   Lupin        = c("#E26A40", "#0E263C", "#3B6183", "#5C85B1", 
                    "#7BA0C6","#D9E2EC"),
   Calamar      = c("#E7E1E4","#F5417C","#040404", "#343334", "#3C3C3C",
@@ -18,6 +19,10 @@ palette_netflyx <- list(
   
   GambitoDama   =c("#291f2a","#634039","#916b58","#be754c","#9a4d1b","#824529"),
   
-  EstamosMuert = c("#c9cacc","#96aea8","#465e69","#5d5e4d","#1c4b46","#8f4031")
+  EstamosMuert = c("#c9cacc","#96aea8","#465e69","#5d5e4d","#1c4b46","#8f4031"),
+  
+  crown        =c("#b08832","#e5bc62","#e6dc96","#8c9490","#545c58","#1e2326"),
+  
+  CobraKai     =c("#292b42","#465d89","#c0c9d0","#f8da6a","#de822e","#6e3d30")
   
 ) 
